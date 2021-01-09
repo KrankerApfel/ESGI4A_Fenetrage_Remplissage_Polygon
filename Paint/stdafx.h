073f4e275f01f1599a1d4053da41b8275ed07a39
Tom <t.rakoto76@gmail.com>
2021-01-09 18:19:35 +0100

    Add lib, include and visual studio configuration

---
+++ Paint/stdafx.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <array>
+#include <vector>
+
+#include <cstdlib>
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
