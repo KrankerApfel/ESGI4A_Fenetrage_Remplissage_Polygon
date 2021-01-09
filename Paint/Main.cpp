aa13707b4ca268f20425074d2381a5dcf7523c60
Tom <t.rakoto76@gmail.com>
2021-01-09 18:45:32 +0100

    test config

--- Paint/Main.cpp
+++ Paint/Main.cpp
@@ -1,8 +1,39 @@
 #include "stdafx.h"
 
-
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 int main()
 {
-    
-  
+
+
+	if (!glfwInit()) { return -1; }
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+
+
+
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+
+
+	}
+
+
+	return 0;
 }
