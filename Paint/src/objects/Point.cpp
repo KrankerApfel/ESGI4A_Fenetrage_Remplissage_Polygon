933691a6866dfdf12022d4d8d83aab6fd1bd4d35
Tom <t.rakoto76@gmail.com>
2021-01-10 20:43:11 +0100

    Add points class

---
+++ Paint/src/objects/Point.cpp
@@ -0,0 +1,6 @@
+#include "Point.h"
+
+double Point::getX() { return this->_x; }
+double Point::getY() { return this->_y; }
+void Point::setX(const double x) { this->_x = x; }
+void Point::setY(const double y) { this->_y = y; }	
---
+++ Paint/src/objects/Point.h
@@ -0,0 +1,10 @@
+#pragma once
+class Point {
+private:
+    double _x, _y;
+public:
+    double getX();
+    double getY();
+    void setX(const double);
+    void setY(const double);
+};
